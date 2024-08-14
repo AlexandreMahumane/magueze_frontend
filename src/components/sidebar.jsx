df0a386c50ef375737775028efbf62ee471638cc
francollas <frnbjr@gmail.com>
2024-08-14 20:07:43 +0200

    Upgrade do sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,16 +1,15 @@
-// src/components/Sidebar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div className="w-56 h-screen bg-base-200 p-4">
-      <ul className="menu bg-base-200 rounded-box w-full">
-        <li>
-          <Link to="/page1" className="flex items-center space-x-2">
+    <div className="w-56 h-screen bg-gradient-to-b from-blue-800 to-blue-500 p-4 shadow-lg">
+      <ul className="menu bg-transparent rounded-box w-full text-white">
+        <li className="mb-4">
+          <Link to="/page1" className="flex items-center space-x-3 hover:bg-blue-700 p-2 rounded-lg transition-colors duration-300">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-7 w-7"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -19,17 +18,17 @@ const Sidebar = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                d="M14 10V4a2 2 0 10-4 0v6a5 5 0 105 5 5 5 0 00-5-5z"
               />
             </svg>
-            <span>Item 2</span>
+            <span className="text-lg font-medium">Temperatura</span>
           </Link>
         </li>
-        <li>
-          <Link to="/page2" className="flex items-center space-x-2">
+        <li className="mb-4">
+          <Link to="/page2" className="flex items-center space-x-3 hover:bg-blue-700 p-2 rounded-lg transition-colors duration-300">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-7 w-7"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -41,14 +40,14 @@ const Sidebar = () => {
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <span>Item 1</span>
+            <span className="text-lg font-medium">Trajetória do Sol</span>
           </Link>
         </li>
         <li>
-          <Link to="/page3" className="flex items-center space-x-2">
+          <Link to="/page3" className="flex items-center space-x-3 hover:bg-blue-700 p-2 rounded-lg transition-colors duration-300">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-7 w-7"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -60,7 +59,7 @@ const Sidebar = () => {
                 d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            <span>Item 3</span>
+            <span className="text-lg font-medium">Velocidade do Vento</span>
           </Link>
         </li>
       </ul>
@@ -69,4 +68,6 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+
 
